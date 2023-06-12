16a4e4072039e976164fba29bf8faf0e1414d3e6
PatriciaMkoji <keyshotrisha@gmail.com>
2023-06-12 23:02:51 +0000

    3.correct requirement fail

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-def print_reversed_list_integer(my_list=[]):
+
+
+def print_reversed_list_integer(my_list = []):
     reversed_list = my_list[::-1]
     for y in reversed_list:
         print("{}".format(y))

28799e174808f3d6d869da33bbe637efefd239d5
PatriciaMkoji <keyshotrisha@gmail.com>
2023-06-12 18:17:14 +0000

    5. Can you C me now?

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def no_c(my_string):
+     new_mystring = ''
+     for char in my_string:
+         if char != 'c' and char != 'C':
+             new_mystring = new_mystring + char
+     return new_mystring

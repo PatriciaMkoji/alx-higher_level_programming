56b275620af9e40cca4f97748a8df99ad14b6eb1
PatriciaMkoji <keyshotrisha@gmail.com>
2023-06-14 16:08:11 -0700

    15. Delete by value

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 def complex_delete(a_dictionary, value):
     del_key = list(map(lambda itm: itm[0], filter(lambda itm: itm[1] == value, a_dictionary.items())))
     for key in del_key:

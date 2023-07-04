f0547741447e4f2375ef656cd1802a9ce3175652
PatriciaMkoji <keyshotrisha@gmail.com>
2023-07-04 13:42:07 -0700

    documentation

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,5 +1,18 @@
 #!/usr/bin/python3
 def say_my_name(first_name, last_name=""):
+     """
+    module prints a person's name, including both 1st name and last name if provided.
+
+    Args:
+        first_name (str): 1st name of the person.
+        last_name (str, optional): The last name of the person. Defaults to an empty string.
+
+    Raises:
+        TypeError: If first_name or last_name is not a string.
+
+    Returns:
+        None
+    """
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):

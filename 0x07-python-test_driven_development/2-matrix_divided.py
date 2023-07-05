c3ba4b8de30b389f33267af23e3f6fd8d6797f5c
PatriciaMkoji <keyshotrisha@gmail.com>
2023-07-05 08:47:19 -0700

    correction on documentation

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -4,19 +4,18 @@ def matrix_divided(matrix, div):
     Divides all elements of a matrix by a given number.
 
     Args:
-        matrix (list of lists): The matrix containing integers or floats.
-        div (int or float): The number to divide the matrix elements by.
+        matrix: The matrix containing integers or floats.
+        div: The number to divide the matrix elements by.
 
     Returns:
-        list of lists: A new matrix with elements divided by div, rounded to 2 decimal places.
+        new_matrix: elements divided by div, rounded to 2 decimal places.
 
     Raises:
-        TypeError: If matrix is not a list of lists of integers or floats, or if div is not a number.
+        TypeError: matrix must be a matrix (list of lists) of integers/floats
         TypeError: If each row of the matrix does not have the same size.
-        ZeroDivisionError: If div is equal to 0.
-
-    """
-    # Validate the matrix
+        ZeroDivisionError: If div is equal to 0
+        """
+    """ Validate the matrix """
     if not isinstance(matrix, list) or not all(isinstance(row, list) for row in matrix):
         raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
 
@@ -27,14 +26,14 @@ def matrix_divided(matrix, div):
     if not all(len(row) == row_size for row in matrix):
         raise TypeError("Each row of the matrix must have the same size")
 
-    # Validate the divisor
+    """ Validate the divisor """
     if not isinstance(div, (int, float)):
         raise TypeError("div must be a number")
 
     if div == 0:
         raise ZeroDivisionError("division by zero")
 
-    # Divide the matrix elements and round to 2 decimal places
+    """ Divide the matrix elements and round to 2 decimal places """
     new_matrix = [[round(element / div, 2) for element in row] for row in matrix]
 
     return new_matrix

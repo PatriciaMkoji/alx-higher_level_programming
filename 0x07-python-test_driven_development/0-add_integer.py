8957719a2591bcb5565aaec1632036c377d0ce43
PatriciaMkoji <keyshotrisha@gmail.com>
2023-07-05 06:33:04 -0700

    NaN value included

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" 
+"""
     A module that adds 2 integers
 
     Args:
@@ -12,10 +12,11 @@
         TypeError: If a or b is not an integer or float.
 """
 
+
 def add_integer(a, b=98):
-    if not isinstance(a, (int, float)):
+    if not isinstance(a, (int, float)) or a != a:
         raise TypeError("a must be an integer")
-    if not isinstance(b, (int, float)):
+    if not isinstance(b, (int, float))b != b:
         raise TypeError("b must be an integer")
     a = int(a)
     b = int(b)

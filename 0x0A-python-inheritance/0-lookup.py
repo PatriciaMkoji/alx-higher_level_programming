66ee4b9a4e4c8ee2a0bb2b1f739d563933b9b9e6
PatriciaMkoji <keyshotrisha@gmail.com>
2023-07-10 05:56:02 -0700

    Requirement fail

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -4,7 +4,7 @@ def lookup(obj):
     Module returns a list of available attributes & methods of an object
 
     Args:
-        self: look into its own object
+    obj: look into its own object
 
     Returns:
         listof attributes& methods of the object

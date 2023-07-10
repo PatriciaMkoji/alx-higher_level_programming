2c6f40f538070b4db4caf36b1b522b337e45f2e0
PatriciaMkoji <keyshotrisha@gmail.com>
2023-07-10 09:14:41 -0700

    Requirement fail

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def lookup(obj):
     """
-    Module returns a list of available attributes & methods of an object
-
+    Returns the list of available attributes and methods of an object
     """
     return dir(obj)

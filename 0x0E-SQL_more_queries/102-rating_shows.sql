15b4a27dc22f6e74c328fc26cd3eab8185e14f9b
PatriciaMkoji <keyshotrisha@gmail.com>
2023-08-18 02:57:55 +0000

    Advave

---
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -0,0 +1,9 @@
+-- Import the database hbtn_0d_tvshows_rate dump to your MySQL
+-- script that lists all shows from
+-- hbtn_0d_tvshows_rate by their rating.
+SELECT `title`, SUM(`rate`) AS `rating`
+  FROM `tv_shows` AS t
+       INNER JOIN `tv_show_ratings` AS r
+       ON t.`id` = r.`show_id`
+ GROUP BY `title`
+ ORDER BY `rating` DESC;

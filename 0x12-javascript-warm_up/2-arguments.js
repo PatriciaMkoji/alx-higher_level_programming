11c5f9cef7f22f7720c1c189f0c5a44f194dfaa6
PatriciaMkoji <keyshotrisha@gmail.com>
2023-09-12 01:53:56 +0000

    semistandard correction

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 if (process.argv.length === 2) {
-	return (console.log('No argument'));
+  console.log('No argument');
 } else if (process.argv.length === 3) {
-	return (console.log('Argument found'));
+  console.log('Argument found');
 } else {
-	return (console.log('Arguments found'));
+  console.log('Arguments found');
 }

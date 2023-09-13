b19a9d957f104332a67f32dbb2b87c62b98e5694
PatriciaMkoji <keyshotrisha@gmail.com>
2023-09-13 13:24:01 -0700

    Rectangle #3

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (w <= 0 || h <= 0 || typeof w !== 'number' || typeof h !== 'number') {
+      return {};
+    } else {
+	this.width = w;
+	this.wheight = h;
+    }
+  }
+  print() {
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+}
+module.exports = Rectangle;

7fe35aeb6c885114158f9f1e2836399c0e5ec7d1
PatriciaMkoji <keyshotrisha@gmail.com>
2023-09-12 19:38:41 -0700

    semi correction

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 class Rectangle {
-	width;
-	height;
+  width;
+  height;
 
-	constructor(w, h) {
-		this.width = w;
-		this.height = h;
-	}
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
+  }
 }
 module.exports = Rectangle;

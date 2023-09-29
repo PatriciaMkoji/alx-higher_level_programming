1132443440de0fa623e127716be6fa6afc61a493
PatriciaMkoji <keyshotrisha@gmail.com>
2023-09-29 16:32:58 -0700

    0. What's my status? #0

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" header """
+if __name__ == "__main__":
+    import urllib.request
+
+    with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as response:
+
+        print("Body response:")
+
+        resp = response.read()
+        print("\t- type: {}".format(type(response.read())))
+        print("\t- content: {}".format(resp))
+        print("\t- utf8 content: {}".format(resp.decode('utf8')))

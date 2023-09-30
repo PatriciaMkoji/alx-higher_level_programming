f8928d5c5168c1db9157936c49366bb5f8fd6483
PatriciaMkoji <keyshotrisha@gmail.com>
2023-09-29 17:33:08 -0700

    4. What's my status? #1

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" Fetch Status """
+if __name__ == "__main__":
+    import requests
+
+    resp = requests.get('https://intranet.hbtn.io/status')
+    print("Body response:")
+    print("\t- type: {}".format(type(resp.text)))
+    print("\t- content: {}".format(resp.text))
